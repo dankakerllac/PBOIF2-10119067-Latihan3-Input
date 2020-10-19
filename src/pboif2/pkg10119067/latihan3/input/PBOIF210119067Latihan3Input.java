6bda8032895ce4db46013f58087d50dc49469cc4
User <User@DESKTOP-6N6EHH6>
2020-10-19 21:50:51 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan3/input/PBOIF210119067Latihan3Input.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan1.biodata;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+public class PBOIF210119067Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Masukkan nama anda: ");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah "+ nama);
+    }
+    
+}
